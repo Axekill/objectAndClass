7b6371c3839f16d1820ae66cc6ab01fccb1ee6f4
Axekill <andrei3.ru@ya.ru>
2023-02-10 13:43:24 +0300

    Добавил методы : toString, equals, hashCode

--- src/lesson/Book.java
+++ src/lesson/Book.java
@@ -1,14 +1,20 @@
 package lesson;
 
+import java.util.Objects;
+
 public class Book {
     private final String name;
     Author author;
     private int year;
 
-    public Book(String name,Author author, int year) {
-        this.author=author;
+    public Book(String name, Author author, int year) {
+        this.author = author;
         this.name = name;
         this.year = year;
+    }
+
+    public Author getAuthor() {
+        return this.author;
     }
 
     public String getName() {
@@ -21,6 +27,24 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги: " + name + ", " + author + ", год издания: " + year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, year);
     }
 }
 
